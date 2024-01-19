c7bddfb265f6f4b904b8c146173d40a67fb9529b
CptBreeza <cbasicok@gmail.com>
2024-01-19 13:31:54 +0800

    impl enqueue (#4)

--- src/queue.ts
+++ src/queue.ts
@@ -1,9 +1,20 @@
-import { EnqueueOptions, QueueJob, QueueJobId } from "./types.ts";
+import { EnqueueJobResponse, EnqueueOptions, QueueJob } from "./types.ts";
+
+export interface JetQueueOptions {
+  instanceName: string;
+}
 
 export class JetQueue {
-  constructor(private queue: string) {
-    // TODO: implement
-    console.log("constructor", this.queue);
+  private pluginInstance: BreezeRuntime.Plugin;
+
+  constructor(private queue: string, options: JetQueueOptions) {
+    const pluginInstance = BreezeRuntime.plugins[options.instanceName];
+
+    if (!pluginInstance) {
+      throw new Error(`Plugin ${options.instanceName} not found`);
+    }
+
+    this.pluginInstance = pluginInstance;
   }
 
   /**
@@ -17,32 +28,32 @@ export class JetQueue {
    *
    * Enqueue a job with the `default` queue:
    * ```ts
-   * const queue = new JetQueue('default');
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1, user_id: 2 });
    * ```
    *
    * Enqueue a job with the `specified` queue other than `default`:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1, user_id: 2 }, { queue: "specified" });
    * ```
    *
    * Schedule a job to run in 5 minutes:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1 }, { scheduleIn: 5 * 60 });
    * ```
    *
    * Enqueue a job, ensuring that it is unique within the past minute:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * const unique = { period: 60 };
    * await queue.enqueue({ id: 1 }, { unique: unique });
    * ```
    *
    * Enqueue a unique job where the period is compared to the `scheduled_at` timestamp rather than `inserted_at`:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1 }, {
    *   unique: { period: 60, timestamp: "scheduled_at" },
    * });
@@ -50,7 +61,7 @@ export class JetQueue {
    *
    * Enqueue a unique job based only on the queue field, and within multiple states:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1 }, {
    *   unique: {
    *     period: 60,
@@ -68,7 +79,7 @@ export class JetQueue {
    *
    * Enqueue a unique job considering only the queue and specified keys in the args:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ account_id: 1, url: "https://example.com" }, {
    *   unique: {
    *     fields: ["args", "queue"],
@@ -79,7 +90,7 @@ export class JetQueue {
    *
    * Enqueue a unique job considering only specified keys in the meta:
    * ```ts
-   * const queue = new JetQueue("default");
+   * const queue = new JetQueue("default", { instanceName: "jetQueueInstance" });
    * await queue.enqueue({ id: 1, name: "Alice" }, {
    *   meta: { slug: "unique-key" },
    *   unique: { fields: ["meta"], keys: ["slug", "id", "name"] },
@@ -92,11 +103,16 @@ export class JetQueue {
   >(
     args: Readonly<A>,
     options?: Partial<EnqueueOptions<keyof A & string, M>>,
-  ): Promise<QueueJobId> {
-    // TODO: implement
+  ): Promise<EnqueueJobResponse> {
     const { queue = this.queue, ...opts } = options ?? {};
-    console.log("enqueue", queue, args, opts);
-    return await Promise.resolve(1);
+
+    return await this.post<EnqueueJobResponse>("/jobs", {
+      args,
+      options: {
+        queue,
+        ...opts,
+      },
+    });
   }
 
   /**
@@ -118,5 +134,21 @@ export class JetQueue {
       id: 1,
       args: {},
     });
+  }
+
+  private async post<T>(path: string, body: object): Promise<T> {
+    const endpoint = await this.pluginInstance.getEndpoint(`/api${path}`);
+
+    const response = await fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      const errorBody = await response.json();
+      throw new Error("Request failed", { cause: errorBody });
+    }
   }
 }
