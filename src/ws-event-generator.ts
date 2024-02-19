10c03d03319e0efb647d1d808d3b4dd8e4a1f197
CptBreeza <cbasicok@gmail.com>
2024-02-19 13:26:08 +0800

    Fix content-type and send ping after socket is open (#8)
    ## 修正的问题
    1. post 时带上了 content-type
    2. 获得正确的 websocket 地址
    3. 不处理 websocket 地址的 scheme
    4. 当 websocket 连接变为 open 之后才开始 ping
    5. 修复了 ws-event-generator
    
    ------
    
    chunk 的逻辑仍然是不正确的：
    1. 它现在是每取 1 个事件就有 100ms 的超时时间，咱们想要的效果是每取 1 组（batchSize 个）事件有 100ms 的超时时间
    2. 现在即使 buffer 里面没有任何事件，只要超过 100ms，它就会 yield 一个空数组 [] 出去
    
    
    建了一个[任务](https://www.notion.so/byzanteam/chunk-312773e5d0f64b8299edc24bff86fc5b)，专门处理一下，这个
    PR 暂不处理

--- src/ws-event-generator.ts
+++ src/ws-event-generator.ts
@@ -1,27 +1,75 @@
-export async function* listen<E>(
-  socket: WebSocket,
-): AsyncIterable<MessageEvent<E>> {
-  const waitList: Array<(event: MessageEvent<E>) => void> = [];
-  const buffer: Array<MessageEvent<E>> = [];
+export interface MessagesStreamOptions<T> {
+  timeout: number;
+  batchSize: number;
+  batchTimeout: number;
+  dataBuilder: (event: MessageEvent<string>) => Array<T>;
+}
 
-  function pushEvent(event: MessageEvent<E>) {
-    if (0 === waitList.length) {
-      buffer.push(event);
-    } else {
-      const resolver = waitList.shift()!;
-      resolver(event);
+export async function* messagesStream<T>(
+  socket: WebSocket,
+  options: MessagesStreamOptions<T>,
+): AsyncIterable<Array<T>> {
+  const { timeout, batchSize, batchTimeout, dataBuilder } = options;
+
+  const buffer: Array<T> = [];
+
+  let pong = true;
+
+  let beginResolver: (() => void) | undefined = undefined;
+  let commitResolver: (() => void) | undefined = undefined;
+
+  function pushEvent(event: MessageEvent<string>) {
+    if (beginResolver) {
+      beginResolver();
+    }
+
+    buffer.push(...dataBuilder(event));
+
+    if (commitResolver && buffer.length >= batchSize) {
+      commitResolver();
     }
   }
 
-  socket.addEventListener("message", pushEvent);
+  socket.addEventListener("open", () => {
+    (function ping() {
+      if (pong) {
+        pong = false;
+        socket.send("ping");
+        setTimeout(ping, timeout);
+      } else {
+        throw new Error("Ping timeout");
+      }
+    })();
+  });
+
+  socket.addEventListener("message", (event: MessageEvent<string>) => {
+    if ("pong" === event.data) {
+      pong = true;
+    } else {
+      pushEvent(event);
+    }
+  });
 
   while (true) {
-    if (0 === buffer.length) {
-      await new Promise<MessageEvent<E>>((resolve) => {
-        waitList.push(resolve);
-      });
-    } else {
-      yield buffer.shift()!;
+    const commit = new Promise<void>((resolve) => {
+      commitResolver = resolve;
+    });
+
+    await new Promise<void>((resolve) => {
+      beginResolver = resolve;
+    });
+
+    beginResolver = undefined;
+
+    await Promise.race([
+      new Promise<void>((resolve) => setTimeout(resolve, batchTimeout)),
+      commit,
+    ]);
+
+    commitResolver = undefined;
+
+    if (0 !== buffer.length) {
+      yield buffer.splice(0, Math.min(buffer.length, batchSize));
     }
   }
 }
